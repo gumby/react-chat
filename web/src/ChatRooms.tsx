ced0b924d54d242acc99c18b11dbdd4ebc539d13
Patrick Gombola <patrick.gombola@gmail.com>
2022-04-07 14:48:08 -0400

    Add room creation form

--- web/src/ChatRooms.tsx
+++ web/src/ChatRooms.tsx
@@ -1,7 +1,5 @@
-import { rm } from "fs";
-import React, { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
-import { json } from "stream/consumers";
+import React, { FormEvent, useEffect, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
 type Room = {
     name: string;
@@ -9,10 +7,11 @@ type Room = {
 
 const ChatRooms = () => {
 
+    const navigate = useNavigate();
     const [rooms, setRooms] = useState<Array<Room>>([]);
+    const [newRoom, setNewRoom] = useState('');
 
     useEffect(() => {
-
         const fetchData = async () => {
             type RoomsResponse = {
                 rooms: Array<Room>;
@@ -29,19 +28,44 @@ const ChatRooms = () => {
             setRooms(jsonResponse.rooms);
         }
         fetchData();
-    }, 
-    [])
+    },
+        [])
 
-    return(
+    const handleCreate = (e: FormEvent) => {
+        e.preventDefault();
+        navigate(`/chat/${newRoom}`)
+    }
+
+    return (
         <>
             <h2>Rooms</h2>
             <ul>
-                {rooms.map((r: Room) => 
+                {rooms.map((r: Room) =>
                     <li key={r.name}>
                         <Link to={`/chat/${r.name}`}>{r.name}</Link>
                     </li>
                 )}
             </ul>
+            <form onSubmit={handleCreate}>
+                <div className="row">
+                    <div className='col input-group w-25 m-3'>
+                        <input
+                            type='input'
+                            className='form-control'
+                            placeholder='Create a room'
+                            value={newRoom}
+                            onChange={(e) => setNewRoom(e.target.value)}
+                        />
+                    </div>
+                    <div className="col">
+                        <button
+                            className='btn mt-3 btn-outline-secondary'
+                            type='submit'>
+                            Create
+                        </button>
+                    </div>
+                </div>
+            </form>
 
         </>
     )
