6483351710519262a52f3da4089faa65c358eedb
Patrick Gombola <patrick.gombola@gmail.com>
2022-04-07 14:46:18 -0400

    Adjust test to use App2

--- web/src/App.test.tsx
+++ web/src/App.test.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from 'react';
+import App2 from './App2';
 
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  render(<App2 />);
+  const linkElement = screen.getByText(/Login To Chat!/i);
   expect(linkElement).toBeInTheDocument();
 });
